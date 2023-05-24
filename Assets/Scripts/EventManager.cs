da48a171dd5beaf31174fc61866127842e97cde8
Opisthoteuthidae <116448496+Opisthoteuthidae@users.noreply.github.com>
2023-05-24 10:46:03 +0300

    Update EventManager.cs
    Создал еще ивентов, мысли насчет текста

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -35,7 +35,16 @@ public class EventManager : MonoBehaviour
         GameEvent eventToSpawn = gameEvents[randomIndex];
 
         // Создаем экземпляр игрового события
-        Instantiate(eventToSpawn.eventPrefab, transform.position, transform.rotation);
+        GameObject eventInstance = Instantiate(eventToSpawn.eventPrefab, transform.position, transform.rotation);
+
+        // Проверяем, является ли событие объектом с компонентом, отображающим текст
+        EventTextDisplay eventTextDisplay = eventInstance.GetComponent<EventTextDisplay>();
+        if (eventTextDisplay != null)
+        {
+            // Выбираем случайное описание события из массива
+            string randomDescription = eventToSpawn.eventDescriptions[Random.Range(0, eventToSpawn.eventDescriptions.Length)];
+            eventTextDisplay.ShowEventText(randomDescription, eventToSpawn.displayDuration);
+        }
     }
 }
 
@@ -43,12 +52,54 @@ public class EventManager : MonoBehaviour
 public class GameEvent
 {
     public GameObject eventPrefab; // Префаб игрового события
+    public string[] eventDescriptions; // Массив текстовых описаний для события
+    public float displayDuration = 2f; // Продолжительность отображения текста
     // Дополнительные параметры и логика события
+    // ...
 }
 
 [System.Serializable]
 public class AmbulanceEvent : GameEvent
 {
-    
+    // Дополнительные параметры и логика для ивента "Скорая помощь"
+    // ...
 }
 
+[System.Serializable]
+public class FirefightersEvent : GameEvent
+{
+    // Дополнительные параметры и логика для ивента "Пожарные"
+    // ...
+}
+
+[System.Serializable]
+public class PoliceEvent : GameEvent
+{
+    // Дополнительные параметры и логика для ивента "Полиция"
+    // ...
+}
+
+public class EventTextDisplay : MonoBehaviour
+{
+    public TextMesh eventText; // Ссылка на компонент TextMesh для отображения текста
+
+    private void Start()
+    {
+        eventText.gameObject.SetActive(false); // Изначально скрываем текст
+    }
+
+    public void ShowEventText(string text, float duration)
+    {
+        eventText.text = text; // Устанавливаем текст, который нужно отобразить
+        eventText.gameObject.SetActive(true); // Показываем текст
+
+        // Запускаем корутину, чтобы скрыть текст через заданную продолжительность
+        StartCoroutine(HideEventTextCoroutine(duration));
+    }
+
+    private IEnumerator HideEventTextCoroutine(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        eventText.gameObject.SetActive(false); // Скрываем текст
+    }
+}
